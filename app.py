1ddf7a6cbe2a8f163e159d1f7c54fa1de613bbd6
Vityshha <vitysha.yakunin@gmail.com>
2025-03-24 20:39:58 +0300

    add ui files

---
+++ app.py
@@ -0,0 +1,16 @@
+# import sys
+# from PyQt5.QtWidgets import QApplication
+#
+#
+#
+# class Application:
+#     def __init__(self):
+#         pass
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     application = Application()
+#     application.view.show()
+#     sys.exit(app.exec_())
+
+
